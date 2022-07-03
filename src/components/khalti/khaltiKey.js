930ccb2668191a051bd0aaec02c42b7037a69429
Suman <sumanniroula720@gmail.com>
2022-07-03 15:04:18 +0545

    delete khalti

--- src/components/khalti/khaltiKey.js
+++
@@ -1,6 +0,0 @@
-let myKey = {
-    publicTestKey: "test_public_key_1efd9300d2a040dcadff7fea1cf09444",
-    secretKey: "test_secret_key_f7a8422dc3bc4eb6a92f4b2f3689eb10",
-  };
-  
-  export default myKey;
